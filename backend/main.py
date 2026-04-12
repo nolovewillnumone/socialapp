9e0d970cb943ed19fb2ff152400fec8e181fe37c
Nolove <saidislombekshukurov@gmail.com>
2026-04-12 19:59:12 +0500

    add missing import os

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 import httpx
+import os
 
 from .database import engine, get_db, Base
 from . import models, schemas, auth
