5aea79d8945a28c7367cd4ab894f7013ec80258f
Saidislombek <ishukurov177@gmail.com>
2026-09-23 22:15:19 +0500

    add scroll-triggered career video section

--- frontend/karta-talantov/src/pages/HomePage.jsx
+++ frontend/karta-talantov/src/pages/HomePage.jsx
@@ -1026,7 +1026,7 @@ function VideoSection({ lang, dark }) {
         {playing ? (
           <iframe
             width="100%" height="100%"
-            src={`https://www.youtube.com/embed/${https://www.youtube.com/watch?v=tXxHxX7PKf8}?autoplay=1&rel=0`}
+            src={`https://www.youtube.com/embed/${"https://www.youtube.com/watch?v=tXxHxX7PKf8}?autoplay=1&rel=0`"}
             title="Career video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
