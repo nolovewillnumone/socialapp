14b6ef96ad67c495466af9726b2d9ded3507c8ca
Saidislombek <ishukurov177@gmail.com>
2026-09-23 22:23:46 +0500

    fix video section template literal build error

--- frontend/karta-talantov/src/pages/HomePage.jsx
+++ frontend/karta-talantov/src/pages/HomePage.jsx
@@ -1026,7 +1026,7 @@ function VideoSection({ lang, dark }) {
         {playing ? (
           <iframe
             width="100%" height="100%"
-            src={`https://www.youtube.com/embed/${'https://www.youtube.com/watch?v=tXxHxX7PKf8}?autoplay=1&rel=0`'}
+            src={"https://www.youtube.com/embed/"+videoId+"?autoplay=1&rel=0"}
             title="Career video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -1037,7 +1037,7 @@ function VideoSection({ lang, dark }) {
           // Thumbnail with play button
           <div
             onClick={() => setPlaying(true)}
-            style={{ width:"100%", height:"100%", cursor:"pointer", position:"relative", background:`url(https://img.youtube.com/vi/${videoId}/maxresdefault.jpg) center/cover` }}
+            style={{ width:"100%", height:"100%", cursor:"pointer", position:"relative", backgroundImage:"url(https://img.youtube.com/vi/"+videoId+"/maxresdefault.jpg)", backgroundSize:"cover", backgroundPosition:"center" }}
           >
             {/* Dark overlay */}
             <div style={{ position:"absolute", inset:0, background:"rgba(0,0,0,0.35)", display:"flex", alignItems:"center", justifyContent:"center" }}>
